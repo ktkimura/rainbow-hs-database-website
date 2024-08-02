86e94545ff504a33368aa0e8cab14a81cdd19bc6
Katie Kimura <kimuraka@flip2.engr.oregonstate.edu>
2024-08-01 17:31:45 -0700

    homepage completed

--- app.js
+++ app.js
@@ -8,40 +8,36 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 9124;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 4021;                 // Set a port number at the top so it's easy to change in the future
 
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');         // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));      // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                     // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 /*
     ROUTES
 */
+
 app.get('/', function(req, res)
-    {
-        // Define our queries
-        query1 = 'DROP TABLE IF EXISTS diagnostic;';
-        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working for ONIDhere!")'; //replace with your onid
-        query4 = 'SELECT * FROM diagnostic;';
+    {  
+        res.render('index');
+    });    
 
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
 
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
+app.get('/students', function(req, res)
+    {  
+        let query1 = "SELECT studentID AS 'Student ID', firstName AS 'First Name', lastName AS 'Last Name', gradClassID AS 'Graduating Class' FROM Students;"             
 
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
-
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
-
-                        // Send the results to the browser
-                        res.send(JSON.stringify(results));
-                    });
-                });
-            });
-        });
-    });
+            res.render('students', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });                                                         // received back from the query
 
 /*
     LISTENER
