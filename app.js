4e4af8a3fcf1bfddf0f8bb49d226657344d695f1
Katie Kimura <kimuraka@classwork.engr.oregonstate.edu>
2024-08-09 18:18:45 -0700

    port change

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 4020;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 4021;                 // Set a port number at the top so it's easy to change in the future
 
 // Helpers
 const dateFormat = require('handlebars-dateformat');            // for formatting eventDate in MM/DD/YYYY
