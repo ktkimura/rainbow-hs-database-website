9f3210d393cbb18c8d8f63a042935d97dc6f98fd
Katie Kimura <kimuraka@oregonstate.edu>
2025-04-25 15:20:59 -0700

    updated app.js for render connectivity

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 */
 var express = require('express');   
 var app     = express();            
-PORT        = 4021;                 
+PORT        = process.env.PORT || 4021;  
 
 /*
     HELPERS
@@ -60,8 +60,22 @@ app.listen(PORT, function(){
 /* 
     DATABASE
 */
-var db = require('./database/db-connector');
+const mysql = require('mysql2');               
 
+const db = mysql.createConnection({
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+  
+  db.connect((err) => {
+    if (err) {
+      console.error('error connecting to the database:', err.stack);
+    } else {
+      console.log('connected to the database');
+    }
+  });
 /*
     ROUTES
 */
