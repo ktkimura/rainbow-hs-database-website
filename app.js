d48d52244f64029c6bb7f04da283e96646671437
Katie Kimura <kimuraka@oregonstate.edu>
2025-04-25 18:37:05 -0700

    change db connection type

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.listen(PORT, function () {
 */
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
