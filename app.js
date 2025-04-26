9a85bb34927c6b4dac71db0013ffe91ff4a57256
Katie Kimura <kimuraka@oregonstate.edu>
2025-04-25 20:26:57 -0700

    trying out vercel

--- app.js
+++ app.js
@@ -9,6 +9,7 @@
 var express = require("express");
 var app = express();
 PORT = process.env.PORT || 3000;
+const path = require("path");
 
 /*
     HELPERS
@@ -43,9 +44,16 @@ Handlebars.registerHelper({
 // Express and handlebars imports
 const { engine } = require("express-handlebars");
 var exphbs = require("express-handlebars");
+
+app.set('views', path.join(__dirname, 'views'));
+
 app.engine(
   ".hbs",
-  engine({ extname: ".hbs", helpers: { dateFormat: dateFormat } })
+  engine({ 
+    extname: ".hbs", 
+    defaultLayout: 'main',
+    helpers: { dateFormat: dateFormat } 
+  })
 );
 app.set("view engine", ".hbs"); // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
@@ -79,21 +87,13 @@ const db = mysql.createPool({
   waitForConnections: true
 });
 
-// db.connect((err) => {
-//   if (err) {
-//     console.error("error connecting to the database:", err.stack);
-//     console.log("host: " + process.env.MYSQL_HOST)
-//   } else {
-//     console.log("connected to the database");
-//   }
-// });
 /*
     ROUTES
 */
 
 // Home page
 app.get("/", function (req, res) {
-  res.render("main");
+  res.render("index");
 });
 
 // Citation for students route functionality:
